79d1a965126f1a133740066fd7b4f1fe14176067
Ardavan Zareirad <ardavan2525@gmail.com>
2023-03-11 20:08:03 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,53 @@
+function add() {
+    // we'll fill this in shortly
+  }
+  function subtract() {
+  }
+  function multiply() {
+  }
+  function divide() {
+  }
+  function add() {
+    return "something";
+  }
+  function add(a, b) {
+    return "something";
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+
+  function increment(n) {
+    return n=n+1;
+  }
+
+  function decrement(n) {
+    return n=n-1;
+  }
+
+  function makeInt(string) {
+    return parseInt(string,10)  
+  }
+
+  function preserveDecimal(string) {
+    return parseFloat(string,10)  
+  }
+  
+
+
+
+
+
+
+
+
+ 
